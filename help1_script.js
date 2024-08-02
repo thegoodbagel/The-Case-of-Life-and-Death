960af53cf8dd05e3e1176ed7a76ec37f0124cc6e
Abrianna Zhang <15221656-thegoodbagel@users.noreply.replit.com>
2024-08-02 00:53:44 +0000

    Initial commit

---
+++ help1_script.js
@@ -0,0 +1,17 @@
+// trigger this function every time the user scrolls
+window.onscroll = function (event) {
+  var section1 = document.getElementById("help1_content");
+  var rect = section1.getBoundingClientRect();
+  
+  var navbar = document.getElementById("nav_bar");
+
+  var sectiontop = rect.top;
+
+  if (sectiontop <= 40){
+    navbar.style.backgroundColor = "var(--cream)";
+    navbar.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0.2)";
+  } else{
+    navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    navbar.style.boxShadow = "0px 8px 16px 0px rgba(0,0,0,0)";
+  }
+}
